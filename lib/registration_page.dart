9b4e2f2be4781f0040e6bd54c017749cfc77c82e
Dilshan Theekshana <theekshanadilshan758@gmail.com>
2025-03-31 12:13:10 +0530

    full code corrected

--- lib/registration_page.dart
+++ lib/registration_page.dart
@@ -23,7 +23,7 @@ class RegistrationPageState extends State<RegistrationPage> {
 
       try {
         final response = await http.post(
-          Uri.parse("http://192.168.170.152/flutter_API/register.php"),
+          Uri.parse("http://localhost/flutter_API/register.php"),
           headers: {"Content-Type": "application/json"},
           body: jsonEncode(
               {"name": _name, "email": _email, "password": _password}),
